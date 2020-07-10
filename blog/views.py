c3c2a8c939af0e4430886152f4a5ae8466d240f7
trevorsidewand <67762179+trevorsidewand@users.noreply.github.com>
2020-07-10 12:35:04 -0400

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,37 @@
 from django.shortcuts import render
-from .models import Post
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.views import View
 
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
-def home(request):
-    context = {
+    def __str__(self):
+        return self.title
+
+class MyTemplateView(View)
+    def home(request):
+        context = {
         'posts': Post.objects.all()
-    }
-    return render(request,'blog/home.html', context)
+        }
+        return render(request,'blog/home.html', context)
 
-def about(request):
-    return render(request,'blog/about.html',{'title': 'About'})
+    def about(request):
+        return render(request,'blog/about.html',{'title': 'About'})
+
+
+
+
+
+
+
+
+
+
+
+
+
